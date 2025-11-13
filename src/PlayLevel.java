dfe524fce0b7262bad1f5289840c5e4527fb60a2
Alex Londal <alexlondal@gmail.com>
2025-11-13 11:07:32 -0500

    Refactor PlayLevel to add base level playing functionality

--- src/PlayLevel.java
+++ src/PlayLevel.java
@@ -1,7 +1,9 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 
+import engine.core.MarioAgent;
 import engine.core.MarioGame;
 import engine.core.MarioResult;
 
@@ -31,9 +33,52 @@ public class PlayLevel {
         return content;
     }
 
-    public static void main(String[] args) {
+    public static void playBaseLevels(MarioAgent agent) {
         MarioGame game = new MarioGame();
         // printResults(game.playGame(getLevel("../levels/original/lvl-1.txt"), 200, 0));
-        printResults(game.runGame(new agents.robinBaumgarten.Agent(), getLevel("./levels/original/lvl-1.txt"), 20, 0, true));
+        int success = 0;
+        int fail = 0;
+        ArrayList<Integer> failedList = new ArrayList<Integer>();
+
+        for (int lvl = 1; lvl <= 1000; lvl++) {
+            String level = String.format("./levels/ge/lvl-%d.txt", lvl);
+            MarioResult result = game.runGame(agent, getLevel(level), 25, 0, false, 500);
+            printResults(result);
+            if (result.getGameStatus().toString().equals("WIN")) {
+                success++;
+            } else {
+                fail++;
+                failedList.add(lvl);
+            }
+            System.out.println("Current results: " + success + " / " + fail);
+        }
+        System.out.println("Final results: " + success + " / " + fail);
+        System.out.print("Failed Levels: ");
+        for (int l : failedList) {
+            System.out.print(l + " ");
+        }
+        System.out.println(" ");
+    }
+
+    public static void playSingleLevel(MarioAgent agent, int lvl) {
+        MarioGame game = new MarioGame();
+        String level = String.format("./levels/original/lvl-%d.txt", lvl);
+        printResults(game.runGame(agent, getLevel(level), 25, 0, false, 240));
+    }
+
+    public static void main(String[] args) {
+
+        //playBaseLevels(new agents.sergeyPolikarpov.Agent());
+        //playSingleLevel(new agents.robinBaumgarten.Agent(), 3);
+        playBaseLevels(new agents.robinBaumgarten.Agent());
+
+//        MarioAgent agent = new agents.robinBaumgarten.Agent();
+//        for (int iter = 1; iter <= 10000; iter++) {
+//            System.out.println("Iteration #" + iter);
+//            playSingleLevel(agent, 1);
+//        }
+//        MarioGame game = new MarioGame();
+//        String level = String.format("./levels/original/lvl-%d.txt", 1);
+//        printResults(game.runGame(agent, getLevel(level), 25, 0, true));
     }
 }
